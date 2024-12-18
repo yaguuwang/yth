1412d1b5f470c56493d714134c0c397402392e5e
Yaguu Wang <yaguuwang@gmail.com>
2024-12-17 22:44:46 -0800

    first commit

---
+++ content.js
@@ -0,0 +1,6 @@
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'h') {
+    // Redirect to YouTube homepage
+    window.location.href = 'https://www.youtube.com/';
+  }
+});
